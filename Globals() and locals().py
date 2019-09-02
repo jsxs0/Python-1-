d9f041322d686eeea24b443136a171fce589c0b2
Jasveen <44266373+jsxs0@users.noreply.github.com>
2019-09-02 11:20:26 +0530

    Globals() and locals()
    # "globals()" returns a dict
    # with all global variables
    # in the current scope:
    
    >>> globals()
    {...}
    
    # "locals()" does the same
    # but for all local variables
    # in the current scope:
    
    >>> locals()
    {...}

---
+++ Globals() and locals().py
@@ -0,0 +1,15 @@
+#globals()
+#locals()
+#!/usr/bin/python3
+from pprint import pprint
+a=10
+b=20
+def foo():
+    x=30 #x, and y are local variables
+    y-40
+print("locals()={0}".format(locals()))
+print(locals()) #same as calling globals()
+print('*',*80)
+print("locals() == globals? ", locals() == globals())
+print('*',*80)
+foo()
